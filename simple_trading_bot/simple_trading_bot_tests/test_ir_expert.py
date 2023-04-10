999734bcfcffe03f88724139165e9e02b76a17e6
lmpizarro <lmpizarro@gmail.com>
2023-04-10 00:55:01 -0300

    test mod

--- simple_trading_bot/simple_trading_bot_tests/test_ir_expert.py
+++ simple_trading_bot/simple_trading_bot_tests/test_ir_expert.py
@@ -13,24 +13,29 @@ class TestIRExpert(unittest.TestCase):
     TODAY = "2021-01-01"
 
     def setUp(self):
+        self.ggal = 'GGAL'
+        self.dlr = 'DLR'
+        self.maturity = 'MAY23'
+        self.ggal_ticker = f'{self.ggal}/{self.maturity}'
+        self.dlr_ticker = f'{self.dlr}/{self.maturity}'
         self._current_underlier_price = 100.
         self._yfinance_md_feed_mock = MagicMock()
         self._yfinance_md_feed_mock.last_prices.return_value = {
-            'GGAL': self._current_underlier_price,
-            'DO': self._current_underlier_price}
+            self.ggal: self._current_underlier_price,
+            self.dlr: self._current_underlier_price}
 
         self._rofex_proxy_mock = MagicMock()
         self._rofex_proxy_mock.bids.return_value = {
-            'GGALFeb21': mdf.OrderbookLevel(115, 10), 'DOFeb21': mdf.OrderbookLevel(125, 10)}
+            self.ggal_ticker: mdf.OrderbookLevel(115, 10), self.dlr_ticker: mdf.OrderbookLevel(125, 10)}
         self._rofex_proxy_mock.asks.return_value = {
-            'GGALFeb21': mdf.OrderbookLevel(120, 10), 'DOFeb21': mdf.OrderbookLevel(130, 10)}
+            self.ggal_ticker: mdf.OrderbookLevel(120, 10), self.dlr_ticker: mdf.OrderbookLevel(130, 10)}
         self._instrument_expert_mock = MagicMock()
         self._maturity_date = dt.datetime(2021, 6, 30, 0, 0, 0, 0)
         self._instrument_expert_mock.tradeable_rofex_instruments_by_underlier_ticker.return_value = {
-            'GGAL': [Future('GGALFeb21', self._maturity_date, 'GGAL', 100.)],
-            'DO': [Future('DOFeb21', self._maturity_date, 'DO', 1000.)]}
+            self.ggal: [Future(self.ggal_ticker, self._maturity_date, self.ggal, 100.)],
+            self.dlr: [Future(self.dlr_ticker, self._maturity_date, self.dlr, 1000.)]}
         self._instrument_expert_mock.maturities_of_tradeable_tickers.return_value = {
-            'GGALFeb21': 'Feb21', 'DOFeb21': 'Feb21'}
+            self.ggal_ticker: self.maturity, self.dlr_ticker: self.maturity}
         self._ir_expert = ire.IRExpert(
             self._instrument_expert_mock,
             self._rofex_proxy_mock,
@@ -39,20 +44,20 @@ class TestIRExpert(unittest.TestCase):
     @freeze_time(TODAY)
     def test_update_rates_when_there_is_arb_opportunity(self):
         self._ir_expert.update_rates()
-        _, max_taker_rate = self._ir_expert.max_taker_rate('Feb21')
-        _, min_offered_rate = self._ir_expert.min_offered_rate('Feb21')
+        _, max_taker_rate = self._ir_expert.max_taker_rate(self.maturity)
+        _, min_offered_rate = self._ir_expert.min_offered_rate(self.maturity)
         self.assertTrue(max_taker_rate > min_offered_rate)
 
 
     @freeze_time(TODAY)
     def test_update_rates_when_there_is_no_arb_opportunity(self):
         self._rofex_proxy_mock.bids.return_value = {
-            'GGALFeb21': mdf.OrderbookLevel(115, 10), 'DOFeb21': mdf.OrderbookLevel(120, 10)}
+            self.ggal_ticker: mdf.OrderbookLevel(115, 10), self.dlr_ticker: mdf.OrderbookLevel(120, 10)}
         self._rofex_proxy_mock.asks.return_value = {
-            'GGALFeb21': mdf.OrderbookLevel(125, 10), 'DOFeb21': mdf.OrderbookLevel(130, 10)}
+            self.ggal_ticker: mdf.OrderbookLevel(125, 10), self.dlr_ticker: mdf.OrderbookLevel(130, 10)}
         self._ir_expert.update_rates()
-        _, max_taker_rate = self._ir_expert.max_taker_rate('Feb21')
-        _, min_offered_rate = self._ir_expert.min_offered_rate('Feb21')
+        _, max_taker_rate = self._ir_expert.max_taker_rate(self.maturity)
+        _, min_offered_rate = self._ir_expert.min_offered_rate(self.maturity)
         self.assertTrue(max_taker_rate < min_offered_rate)
 
     @freeze_time(TODAY)
@@ -66,11 +71,11 @@ class TestIRExpert(unittest.TestCase):
                            * self._current_underlier_price)
 
         self._rofex_proxy_mock.bids.return_value = {
-            'GGALFeb21': mdf.OrderbookLevel(100, 10), 'DOFeb21': mdf.OrderbookLevel(max_taker_price, 10)}
+            self.ggal_ticker: mdf.OrderbookLevel(100, 10), self.dlr_ticker: mdf.OrderbookLevel(max_taker_price, 10)}
         self._rofex_proxy_mock.asks.return_value = {
-            'GGALFeb21': mdf.OrderbookLevel(min_offered_price, 10), 'DOFeb21': mdf.OrderbookLevel(500, 10)}
+            self.ggal_ticker: mdf.OrderbookLevel(min_offered_price, 10), self.dlr_ticker: mdf.OrderbookLevel(500, 10)}
         self._ir_expert.update_rates()
-        _, max_taker_rate = self._ir_expert.max_taker_rate('Feb21')
-        _, min_offered_rate = self._ir_expert.min_offered_rate('Feb21')
+        _, max_taker_rate = self._ir_expert.max_taker_rate(self.maturity)
+        _, min_offered_rate = self._ir_expert.min_offered_rate(self.maturity)
         self.assertAlmostEqual(fxd_max_taker_rate, max_taker_rate, 10)
         self.assertAlmostEqual(fxd_min_offered_rate, min_offered_rate, 10)
